9016d4c8778b00e649e36129eb4c107d45a48428
alu0100769579 <alu0100769579@ull.edu.es>
2015-11-25 13:59:35 +0000

    Creada estructura de practicas anteriores

---
+++ lib/biblio/class.rb
@@ -0,0 +1,85 @@
+class Libro
+	
+		
+		attr_accessor :autor, :titulo, :serie, :editorial, :edicion, :publicacion
+		def initialize(autor, titulo, serie, editorial, edicion, publicacion)
+			@autor = autor
+			@titulo = titulo
+			@serie = serie
+			@editorial = editorial
+			@edicion = edicion
+			@publicacion = publicacion
+		end
+		def to_s
+		    "#{@autor}\n#{@titulo}\n#{@serie}\n#{@editorial}; #{@edicion} (#{@publicacion})\n"
+		end
+end
+
+class Book < Libro
+	attr_accessor :isbn
+	
+	def initialize(autor, titulo, serie, editorial, edicion, publicacion, isbn)
+		
+		super :autor,:titulo, :serie, :editorial, :edicion, :publicacion
+	
+			@autor = autor
+			@titulo = titulo
+			@serie = serie
+			@editorial = editorial
+			@edicion = edicion
+			@publicacion = publicacion
+			@isbn = isbn
+	end
+	def to_s
+		
+		super :autor,:titulo, :serie, :editorial, :edicion, :publicacion
+	
+		"#{@autor}\n#{@titulo}\n#{@serie}\n#{@editorial}; #{@edicion} (#{@publicacion})\n#{@isbn}"
+	end
+end
+
+class Magazine < Libro
+	
+	attr_accessor :issn
+	
+	def initialize(autor, titulo, serie, editorial, edicion, publicacion, issn)
+		super :autor,:titulo, :serie, :editorial, :edicion, :publicacion
+	
+			@autor = autor
+			@titulo = titulo
+			@serie = serie
+			@editorial = editorial
+			@edicion = edicion
+			@publicacion = publicacion
+			@issn = issn
+	end
+	def to_s
+		super :autor,:titulo, :serie, :editorial, :edicion, :publicacion
+	
+		"#{@autor}\n#{@titulo}\n#{@serie}\n#{@editorial}; #{@edicion} (#{@publicacion})\n#{@issn}"
+	end
+end
+
+
+
+class Elec_Document < Libro
+	
+	attr_accessor :url
+	
+	def initialize(autor, titulo, serie, editorial, edicion, publicacion, url)
+		super :autor,:titulo, :serie, :editorial, :edicion, :publicacion
+		
+			@autor = autor
+			@titulo = titulo
+			@serie = serie
+			@editorial = editorial
+			@edicion = edicion
+			@publicacion = publicacion
+			@url = url
+	end
+	def to_s
+		super :autor,:titulo, :serie, :editorial, :edicion, :publicacion
+	
+		"#{@autor}\n#{@titulo}\n#{@serie}\n#{@editorial}; #{@edicion} (#{@publicacion})\n#{@url}"
+	end
+end
---
+++ lib/biblio/list.rb
@@ -0,0 +1,97 @@
+
+Node = Struct.new(:value, :next)
+Node_ = Struct.new(:prev, :value, :next)
+
+class   List
+   
+    def initialize ()
+           
+        @elemento = Node.new(nil)
+    end
+    
+    def elemento
+        
+        @elemento
+    end   
+    
+    def insertar_elemento(node)
+        
+        if @elemento != nil
+            
+            node.next = @elemento
+            @elemento = node
+        end
+    end
+    
+    def extraer_elemento 
+        
+        aux = @elemento
+        @elemento = @elemento.next
+        aux.value
+    end    
+    
+end
+
+class Lista_doble
+   
+   attr_accessor :head, :tail
+   
+   def empty?
+      
+      @head == nil
+   end
+   
+   def initialize()
+      
+      @head = nil
+      @tail = nil 
+   end
+   
+   def insertar_elemento(node)
+      
+      @node =  Node_.new(nil, node, nil)
+      
+      if @tail == nil
+         @head = @node
+         @tail = @node
+         #@node
+      else
+          @node.next = @head
+          @head.prev = @node
+          @head = @node
+          #@node
+      end
+      
+   end
+   
+   def extraer_elemento
+       
+       @node = Node_.new(nil, @head.value, nil)
+       
+       @head = @head.next
+       @head.prev = nil
+       return @node
+        
+       #node = @head
+       #if @head.next == nil
+      #     @head = nil
+     #  else
+    #       @head = @head.next
+       #end
+       #return node
+       
+       #node = @head
+       #@head = @head.next
+       
+       #node.next = nil
+       #node.prev = nil
+       
+       #if @head == nil
+        #  @tail = nil
+       #end
+       
+       #return node
+   end
+   
+    
+end
